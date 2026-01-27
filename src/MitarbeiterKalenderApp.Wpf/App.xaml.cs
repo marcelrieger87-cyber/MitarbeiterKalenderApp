31c75b53b7b41356d8939bed983d62288755dc8e
marcelrieger87-cyber <marcelrieger87@gmail.com>
2026-01-27 16:11:15 +0100

    Update App.xaml.cs

--- src/MitarbeiterKalenderApp.Wpf/App.xaml.cs
+++ src/MitarbeiterKalenderApp.Wpf/App.xaml.cs
@@ -4,9 +4,4 @@ namespace MitarbeiterKalenderApp.Wpf;
 
 public partial class App : Application
 {
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-        new MainWindow().Show();
-    }
 }
