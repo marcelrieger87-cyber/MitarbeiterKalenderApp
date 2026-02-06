cf754f692586ce9a9ed0492cba365529583def51
marcelrieger87-cyber <marcelrieger87@gmail.com>
2026-02-06 08:30:19 +0100

    Delete Mitarbeiter.Kalender.App directory

--- Mitarbeiter.Kalender.App/ViewModels/EmployeeOption.cs
+++
@@ -1,8 +0,0 @@
-namespace Mitarbeiter.Kalender.App.ViewModels;
-
-public sealed record EmployeeOption(string? Id, string DisplayName)
-{
-    public override string ToString() => DisplayName;
-
-    public static EmployeeOption All { get; } = new(null, "Alle Mitarbeiter");
-}
